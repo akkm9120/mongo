02c06f1ad62b4894bc858f1e6bdcf55467636fa7
Aung Kyaw  Min <mail4akkm@gmail.com>
2024-01-13 04:39:17 +0000

    add files

---
+++ sctp01-mongo/airbnb_demo.js
@@ -0,0 +1,23 @@
+// require in the MongoDB client
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+
+const mongoURL = process.env.MONGO_URL;
+
+
+async function main() {
+ 
+ const client = await MongoClient.connect(mongoURL, {
+    "useUnifiedTopology": true // it's for using the latest version of MongoDB
+ });
+ // connection
+ const db = client.db("sample_airbnb");
+ console.log("database connected");
+ // mongoshell: db.listingsAndReviews.find().limit(10)
+ const listings = await db.collection("listingsAndReviews").find({})
+                  .project({  "name":true, "address.street":1 , "address.country":1,   "property_type":1,   "room_type":1,   "bedrooms":1,   "beds":1,   "price":1})
+                  .limit(10).toArray();
+
+ console.log(listings);
+}
+main();
